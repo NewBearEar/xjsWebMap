3cee58108953bc3f4a137829d4054c9a830a9949
NewBearEar <774205561@qq.com>
2020-06-01 01:37:03 +0800

    实现搜索框实时显示模糊搜索结果，自动补全

--- src/main/src/servletsrc/AutoSearchCompleteServlet.java
+++ src/main/src/servletsrc/AutoSearchCompleteServlet.java
@@ -1,6 +1,8 @@
 package servletsrc;
 
 import javasrc.PostgreUtil;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,15 +12,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 
 @WebServlet(name = "AutoSearchCompleteServlet",urlPatterns = "/autoComplete")
 public class AutoSearchCompleteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println(request.getParameter("searchTxt"));  //注意大小写
+        //解决中文乱码
+        request.setCharacterEncoding("utf-8");
+        // System.out.println(request.getParameter("searchTxt"));  //注意大小写
 
         //尝试连接
         String url = "jdbc:postgresql://47.94.150.127:5432/chn_test";
@@ -30,6 +36,9 @@ public class AutoSearchCompleteServlet extends HttpServlet {
 
         String searchTxt = request.getParameter("searchTxt"); //这里为什么不需要ajax的data 的键写成“searchTxt”
 
+        //为response解决乱码
+        response.setContentType("text/html;charset=UTF-8");
+        response.setCharacterEncoding("utf-8");
         //获取查询
         //查询基础语句,先查询城市中文名试试
         String sqlBase = "select id,name "+
@@ -40,6 +49,34 @@ public class AutoSearchCompleteServlet extends HttpServlet {
             sql = sqlBase + "where name like " + "\'%" + searchTxt + "%\'";    //先支持城市名称的模糊查询试试
             System.out.println(sql);   //看看sql语句
             ResultSet queryResult = PostgreUtil.getResultSet(testConn,sql);
+            //数据库记录转JSON，有时间重构成一个函数
+            JSONArray jsonArray = new JSONArray();
+            ResultSetMetaData resultSetMetaData = null;
+            try {
+                resultSetMetaData = queryResult.getMetaData();
+                int columnCount = resultSetMetaData.getColumnCount();
+                while (queryResult.next()){
+                    JSONObject jsonObject = new JSONObject();
+                    //遍历每一列
+                    for (int i = 1;i<=columnCount;i++){  //强烈注意这里的i从1开始，getColumnLabel从1开始
+                        String columnLabel = resultSetMetaData.getColumnLabel(i);
+                        String key = null;
+                        if("name".equals(columnLabel)){
+                            key = "label";
+                        }else {
+                            key = columnLabel;
+                        }
+                        String value = queryResult.getString(columnLabel);
+                        jsonObject.put(key,value);
+                    }
+                    jsonArray.put(jsonObject);  //向json数组添加jsonObj
+                }
+                //json转String
+                String resultStr = jsonArray.toString();
+                response.getWriter().print(resultStr);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
         }else {
             response.getWriter().print("{}");   //表示没有查询到并返回空json
